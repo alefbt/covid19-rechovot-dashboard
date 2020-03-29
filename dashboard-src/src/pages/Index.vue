c6eacd4d7eb661549fcb1ffdddb8c7e2d18b2bc5
Yehuda Korotkin <yehudako@matrix.co.il>
2020-03-29 15:12:33 +0300

    devops finialize

--- dashboard-src/src/pages/Index.vue
+++ dashboard-src/src/pages/Index.vue
@@ -1,14 +1,30 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+<!--
+  <q-page class="flex flex-center" :style-fn="myTweak" id="page-id-a">
+    <div class="col">
+      TEST
+    </div>
   </q-page>
+-->
+<q-page class="column justify-start">
+</q-page>
 </template>
-
+<style lang="stylus">
+  #page-id-a
+    background-color: #f0f0f0
+</style>
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+   methods: {
+    myTweak (offset) {
+      // "offset" is a Number (pixels) that refers to the total
+      // height of header + footer that occupies on screen,
+      // based on the QLayout "view" prop configuration
+
+      // this is actually what the default style-fn does in Quasar
+      return { minHeight: offset ? `calc(100vh - ${offset}px)` : '100vh' }
+    }
+  }
 }
 </script>
---
+++ scripts/release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# [200~rsync -av /home/pi/covid19-rechovot-dashboard/dist/ /var/www/html/
